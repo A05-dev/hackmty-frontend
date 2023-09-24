8cd6b9d74509db1737feec6ce8c790a9f4ee8904
DanielBrMz <a01254805@tec.mx>
2023-09-24 11:51:53 -0600

    api in flask

--- client/js/image-upload.js
+++ client/js/image-upload.js
@@ -1,30 +1,45 @@
 const dropArea = document.getElementById('drop-area'),
     fileInput = document.getElementById('input-file'),
-    imageView = document.getElementById('img-view');
+    imageView = document.getElementById('img-view'),
+    loadingSvg = document.getElementById('loading'); // Assuming you have an element with id 'loading-svg' for the loading animation
 
 fileInput.addEventListener('change', uploadImage);
 
+loadingSvg.style.display = 'none';
+
 const uploadImage = ()=>{
+  // Show the loading SVG
+  loadingSvg.style.display = 'block';
+
+
   let imgLink = URL.createObjectURL(fileInput.files[0]);
   imageView.style.backgroundImage = `url(${imgLink})`;
   imageView.textContent = '';
   imageView.style.border = 0;
 
-  // Crea un objeto FormData
+  // Create a FormData object
   let formData = new FormData();
 
-  // Agrega el archivo al objeto FormData
+  // Add the file to the FormData object
   formData.append('image', fileInput.files[0]);
 
-  // Define las opciones de la solicitud
+  // Define the request options
   let requestOptions = {
     method: 'POST',
     body: formData,
   };
 
-  // Realiza la solicitud a tu API
+  // Make the request to your API
   fetch('https://api.example.com/analyzeImage', requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('Error:', error));
+    .then(result => {
+      console.log(result);
+      // Hide the loading SVG after the image is uploaded and response is received
+      loadingSvg.style.display = 'none';
+    })
+    .catch(error => {
+      console.log('Error:', error);
+      // Hide the loading SVG if there's an error
+      loadingSvg.style.display = 'none';
+    });
 }
