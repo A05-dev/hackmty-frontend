3d9ce9de31b87abb8716be3b82aa02cbe6b7f4c0
DanielBrMz <a01254805@tec.mx>
2023-09-24 06:21:20 -0600

    conectando a posible api

--- client/js/image-upload.js
+++ client/js/image-upload.js
@@ -2,11 +2,29 @@ const dropArea = document.getElementById('drop-area'),
     fileInput = document.getElementById('input-file'),
     imageView = document.getElementById('img-view');
 
-inputFile.addEventListener('change', (e)=>{ uploadImage});
+fileInput.addEventListener('change', uploadImage);
 
 const uploadImage = ()=>{
-  let imgLink = URL.createObjectURL(inputFile.files(0));
-  imageView.style.baccgroundImage = `url(${imgLink})`;
+  let imgLink = URL.createObjectURL(fileInput.files[0]);
+  imageView.style.backgroundImage = `url(${imgLink})`;
   imageView.textContent = '';
   imageView.style.border = 0;
+
+  // Crea un objeto FormData
+  let formData = new FormData();
+
+  // Agrega el archivo al objeto FormData
+  formData.append('image', fileInput.files[0]);
+
+  // Define las opciones de la solicitud
+  let requestOptions = {
+    method: 'POST',
+    body: formData,
+  };
+
+  // Realiza la solicitud a tu API
+  fetch('https://api.example.com/analyzeImage', requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('Error:', error));
 }
